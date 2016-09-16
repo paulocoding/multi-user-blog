d4e1a5c2594af692a56bef345d44f9044e32b48b
Paulo Silva <engelik@gmail.com>
2016-09-16 16:47:04 +0100

    home-page layout

--- multi-user-blog/main.py
+++ multi-user-blog/main.py
@@ -14,11 +14,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
 import webapp2
+import jinja2
+from google.appengine.ext import db
 
-class MainHandler(webapp2.RequestHandler):
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainHandler(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.render("home.html")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
