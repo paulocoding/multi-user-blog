8365e8b98f916879c3212cc6dd38b6648840d224
Paulo Silva <engelik@gmail.com>
2016-09-19 16:20:08 +0100

    improved main page usability

--- multi-user-blog/main.py
+++ multi-user-blog/main.py
@@ -23,6 +23,15 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
 
+class Post(object):
+    """docstring for Post."""
+    def __init__(self, title, content, author, likes):
+        super(Post, self).__init__()
+        self.title = title
+        self.content = content
+        self.author = author
+        self.likes = likes
+
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
@@ -34,9 +43,21 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+posts = []
+posts.append(Post("Post Title", "Lorem ipsum dolor sit amet, consectetur \
+                  adipisicing elit, sed do eiusmod tempor incididunt ut labore\
+                   et dolore magna aliqua. Ut enim ad minim veniam, quis \
+                   nostrud exercitation ullamco laboris nisi ut aliquip ex ea \
+                   commodo consequat. Duis aute irure dolor in reprehenderit \
+                   in voluptate velit esse cillum dolore eu fugiat nulla \
+                   pariatur. Excepteur sint occaecat cupidatat non proident, \
+                   sunt in culpa qui officia deserunt mollit anim id est \
+                   laborum.", "me", 0))
 class MainHandler(Handler):
     def get(self):
-        self.render("home.html")
+        general_error = ""
+        self.render("home.html", logged=False, general_error=general_error,
+                    posts=posts, user="")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
